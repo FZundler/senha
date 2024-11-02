befa5cb86347d3d67326053257abb73ff7a9cd34
Felipe Zündler <felipezundlercontato@gmail.com>
2024-11-02 19:23:06 -0300

    senha

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import ResetPasswordCard from "@/components/Reset";
+import SocialMediaLinks from "@/components/SocialMediaLinks"; // Importando o componente de redes sociais
+
+export const metadata: Metadata = {
+    title: "Mão na roda",
+    description: "Plataforma que conecta profissionais e clientes",
+    keywords: [""],
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="pt-br">   
+            <body className="flex flex-col min-h-screen"> 
+                <Header />
+                <main className="flex-grow flex items-center justify-center py-64"> {/* Mantém o card centralizado */}
+                    <ResetPasswordCard /> 
+                </main>
+                <SocialMediaLinks /> {/* Adicionando as redes sociais */}
+                <Footer />
+            </body>
+        </html> 
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="w-screen h-screen">
+        <h1>oi</h1>
+      </div>
+    </main>
+  );
+}
